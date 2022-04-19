cc70de699df42fc0f9eff59230abd3e450c59709
Avily Silva <avily.sva@gmail.com>
2022-04-19 10:41:30 -0300

    home page content was created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
-import { Header } from "../components/Header";
+import Head from "next/head";
+
+import styles from "../styles/pages/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+
+          <p>
+            Get access to all publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   )
 }
