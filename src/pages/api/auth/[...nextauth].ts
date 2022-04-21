46f9f3c38922e4eb84f5ded742bf8d59fa898ec6
Avily Silva <avily.sva@gmail.com>
2022-04-21 10:46:39 -0300

    user signin now is sending to fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,39 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query } from "faunadb";
+
+import { faunadb } from "../../../services/faunadb";
 
 export default NextAuth({
+  secret: process.env.JWT_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
         params: {
-          scope: "read:user"
+          scope: "read:user",
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await faunadb.query(
+          query.Create(
+            query.Collection("users"),
+            {
+              data: {
+                email: user.email
+              }
+            }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
