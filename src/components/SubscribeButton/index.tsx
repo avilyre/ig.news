2b852dbd4cdb9972828360c228ee4f9890e3e8f6
Avily Silva <avily.sva@gmail.com>
2022-04-22 15:16:33 -0300

    stripe backend was configured

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,23 @@
+import { signIn, useSession } from "next-auth/react";
+
 import { SubscribeButtonProps } from "./interfaces";
 import styles from "./styles.module.scss";
 
 export function SubscribeButton({ productId }: SubscribeButtonProps) {
+  const { status } = useSession();
+
+  function handleSubscribe() {
+    if (status !== "authenticated") {
+      signIn("github");
+      return;
+    }
+  }
+
+  
   return (
     <button
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
